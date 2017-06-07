47a615795e87fff0eaea95a156d4a3fdc07a9ea6
Marcus1920 <mark.mbayo.n@gmail.com>
2017-06-07 18:41:26 +0200

    update  the  route

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-<<<<<<< HEAD
+
 //
 Route::get('/login','LoginController@login');
 Route::post('login','LoginController@postlogin');
@@ -29,11 +29,8 @@ Route::get('register','RegisterController@registrationForm');
 Route::post('register','RegisterController@registerUser');
 
 
-
-=======
 Route::get('getAll','IterationController@index');
 Route::get('getIteration/{id}','IterationController@show');
 Route::get('storeIteration','IterationController@store');
 Route::get('editIteration/{id}','IterationController@edit');
 Route::get('deleteIteration/{id}','IterationController@destroy');
->>>>>>> 9e596b75f0aaa5d1421d01d6dd0bfca5680f9a6f
